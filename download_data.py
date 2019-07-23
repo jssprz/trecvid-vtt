8e18f719a6149ebb4613cfda6bc3254f9e046cbf
Jesús Pérez-Martín <jperezmartin90@gmail.com>
2019-07-23 15:27:40 -0400

    adds user and password options

--- download_data.py
+++ download_data.py
@@ -15,14 +15,15 @@ __email__ = "jperezmartin90@gmail.com"
 __status__ = "Development"
 
 
-def download_and_process_video(save_dir_path, video_name, video_url):
+def download_and_process_video(save_dir_path, video_name, video_url, user, password):
     full_path = os.path.join(save_dir_path, video_name)
 
     if os.path.exists(full_path):
         return 1
 
     http = urllib3.PoolManager()
-    r = http.request('GET', video_url, preload_content=False)
+    headers = urllib3.util.make_headers(basic_auth='{}:{}'.format(user, password))
+    r = http.request('GET', video_url, headers=headers, preload_content=False)
 
     if r.status >= 300:
         return 2
@@ -39,7 +40,7 @@ def download_and_process_video(save_dir_path, video_name, video_url):
     return 0
 
 
-def download_videos(save_dir_path, list_file_path):
+def download_videos(save_dir_path, list_file_path, user, password):
     with open(list_file_path) as f:
         lines = f.readlines()
         video_data = [{"video_id": data[0], "url": data[1], "video_format": "{}".format(data[1].split('.')[-1])} for data in
@@ -51,7 +52,7 @@ def download_videos(save_dir_path, list_file_path):
     for row in video_data:
         result = download_and_process_video(save_dir_path,
                                             '{}.{}'.format(row['video_id'], row['video_format']),
-                                            row['url'])
+                                            row['url'], user, password)
         if result == 0:
             downloads_count += 1
         elif result == 2:
@@ -63,10 +64,12 @@ def download_videos(save_dir_path, list_file_path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download the videos from a urls.')
-    parser.add_argument('-save_dir_path', '--save', type=str, default='videos',
+    parser.add_argument('-save', '--save_dir_path', type=str, default='videos',
                         help='the path of the folder to save the downloaded videos (default is videos).')
-    parser.add_argument('-list_file_path', '--file', type=str, default='videos_urls.txt',
+    parser.add_argument('-file', '--list_file_path', type=str, default='videos_urls.txt',
                         help='the path of the file with the list of urls to downloaded (default is videos_urls.txt).')
+    parser.add_argument('-u', '--user', type=str, help='the trecvid userid (required for Flickr videos).')
+    parser.add_argument('-p', '--password', type=str, help='the trecvid password (required for Flickr videos).')
 
     args = parser.parse_args()
 
@@ -79,4 +82,4 @@ if __name__ == '__main__':
     logging.info('path to save videos: {}'.format(args.save))
     logging.info('file with the list of URLs: {}'.format(args.file))
 
-    download_videos(args.save, args.file)
+    download_videos(args.save_dir_path, args.list_file_path, args.user, args.password)
